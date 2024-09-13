8ea99f814a13359545800b3e665496fd121159f5
rookieyao <2101093171@qq.com>
2024-09-13 21:38:06 +0800

    init

---
+++ src/main/java/com/rookie/java/tree/JavaTreeApplication.java
@@ -0,0 +1,13 @@
+package com.rookie.java.tree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTreeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rookie/java/tree/JavaTreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rookie.java.tree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTreeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
